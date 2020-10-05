197f157875695d1b3374017bbe8f189822603ab2
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-10-05 17:51:57 -0400

    shapes and prefab updates

---
+++ Assets/Scripts/Structures/DNABase.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+namespace BioTower.Structures
+{
+
+[SelectionBase]
+public class DNABase : Structure
+{   
+
+}
+}
+
--- Assets/Scripts/Structures/Structure.cs
+++ Assets/Scripts/Structures/Structure.cs
@@ -2,18 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using NaughtyAttributes;
+using UnityEngine.UI;
 
 namespace BioTower.Structures
 {
+
+public enum StructureState
+{
+    NONE,
+    ACTIVE,
+    DESTROYED
+}
+
 public class Structure : MonoBehaviour
 {   
     [SerializeField] private bool hasHealth;
     [EnableIf("hasHealth")] [Range(0,100)] [SerializeField] protected int maxHealth;
     [EnableIf("hasHealth")] [SerializeField] protected int currHealth;
+    [EnableIf("hasHealth")] [SerializeField] protected Slider healthSlider;
+    [SerializeField] StructureState structureState;
 
     public virtual void Awake()
     {
         currHealth = maxHealth;
+        structureState = StructureState.ACTIVE;
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currHealth;
+    }
+
+    public virtual void TakeDamage(int numDamage)
+    {
+        if (hasHealth)
+        {
+            currHealth -= numDamage;
+        }
     }
 }
 }

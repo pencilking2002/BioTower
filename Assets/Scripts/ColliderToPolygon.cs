a47b60527407656f92667229399dc19e7cda0ee7
Roman Sharf <pencilking2002@gmail.com>
2020-10-09 20:56:43 -0400

    Put in sprite shapes and test graphics for road and gras

---
+++ Assets/Scripts/ColliderToPolygon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+using Shapes;
+
+public class ColliderToPolygon : MonoBehaviour
+{
+    [SerializeField] private PolygonCollider2D col;
+    [SerializeField] private Polygon polygon;
+
+    [Button("Bake")]
+    private void Bake()
+    {
+        var points = col.points;
+        polygon.SetPoints(points);
+    }
+
+    [Button("Reset Polygon")]
+    private void ResetPolygon()
+    {
+        var arr = new Vector2[0];
+        polygon.SetPoints(arr);
+    }
+}

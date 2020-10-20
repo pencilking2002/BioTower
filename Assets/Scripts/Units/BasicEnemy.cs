bbaaefe789a87a5cd242889d502d3d390d85ef22
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-10-20 13:16:54 -0400

    assembly defs and sortign layer updates

--- Assets/Scripts/Units/BasicEnemy.cs
+++ Assets/Scripts/Units/BasicEnemy.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using PolyNav;
 
 namespace BioTower.Units
 {
 [SelectionBase]
 public class BasicEnemy : Unit
 {
+    [SerializeField] private Transform target;
+    [SerializeField] private PolyNavAgent agent;
 
+    private void Awake()
+    {
+        agent = GetComponent<PolyNavAgent>();
+    }
 }
 }

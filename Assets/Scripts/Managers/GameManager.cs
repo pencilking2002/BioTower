798a3a74cad7497208bdff221d4c85648d170411
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-10-20 15:14:04 -0400

    Created a GameManager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace BioTower
+{
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    [SerializeField] private int numLoads;
+
+    private void Awake()
+    {
+        InitInstance();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            LoadLevel(0);
+        }    
+    }
+
+    private void LoadLevel(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        numLoads++;
+        Debug.Log($"Scene: {scene}. Mode: {mode}. Num Loads: {numLoads}");
+    }
+    
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void InitInstance()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            
+            if (transform.parent != null)
+                DontDestroyOnLoad(gameObject.transform.parent.gameObject);
+            else
+                DontDestroyOnLoad(gameObject);
+        }
+        else
+            Destroy(gameObject);
+    }
+}
+}

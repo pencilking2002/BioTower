0ce65b039ff65e9ddfed1ae208a682aab748c812
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-12-30 11:52:13 -0500

    ABA units are spawning near the aba tower

--- Assets/Scripts/Structures/ABATower.cs
+++ Assets/Scripts/Structures/ABATower.cs
@@ -1,7 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using NaughtyAttributes;
+using System;
+using BioTower.Units;
 
 namespace BioTower.Structures
 {
@@ -9,7 +10,25 @@ namespace BioTower.Structures
 [SelectionBase]
 public class ABATower : Structure
 {   
+    [SerializeField] private int numUnitsToSpawn = 4;
+    [SerializeField] private GameObject abaUnitPrefab;
+    [SerializeField] private CircleCollider2D influenceAreaCollider;
+    [SerializeField] private List<Unit> abaUnits;
 
+    public override void Awake()
+    {
+        base.Awake();
+        
+        for(int i=0; i<numUnitsToSpawn; i++)
+        {
+            var go = Instantiate(abaUnitPrefab);
+            Vector3 circlePos = UnityEngine.Random.insideUnitCircle * influenceAreaCollider.radius;
+            go.transform.position = influenceAreaCollider.transform.position + circlePos;  
+            go.transform.SetParent(transform);
+            var unit = go.GetComponent<Unit>();
+            abaUnits.Add(unit);
+        }
+    }
 }
 }
 
---
+++ Assets/Scripts/Units/AbaUnit.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using PolyNav;
+
+namespace BioTower.Units
+{
+[SelectionBase]
+public class AbaUnit : Unit
+{
+    public PolyNavAgent agent;
+    
+    private void Awake()
+    {
+        //agent.enabled = false;
+    }
+
+    public override void Start()
+    {
+        base.Start();
+    }
+}
+}

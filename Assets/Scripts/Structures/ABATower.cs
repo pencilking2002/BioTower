3734ae0cec093aaf04fbc8546488e0fde7ee05ef
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2020-12-30 19:28:15 -0500

    Started putting in states for aba units

--- Assets/Scripts/Structures/ABATower.cs
+++ Assets/Scripts/Structures/ABATower.cs
@@ -13,7 +13,7 @@ public class ABATower : Structure
     [SerializeField] private CircleCollider2D maxInfluenceAreaCollider;
     [SerializeField] private CircleCollider2D minInfluenceAreaCollider;
     [SerializeField] private int numUnitsToSpawn = 4;
-    [SerializeField] private List<Unit> abaUnits;
+    [SerializeField] private List<AbaUnit> abaUnits;
 
     private Vector3[] targetPositions;
     public override void Awake()
@@ -31,8 +31,21 @@ public class ABATower : Structure
             var go = Instantiate(abaUnitPrefab);
             go.transform.position = GetPointWithinInfluence();
             go.transform.SetParent(unitsContainer);
-            var unit = go.GetComponent<Unit>();
+            var unit = go.GetComponent<AbaUnit>();
             abaUnits.Add(unit);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        for (int i=0; i<numUnitsToSpawn; i++)
+        {
+            var unit = abaUnits[i];
+            if (unit.hasTargetRoamingPoint)
+            {
+                var direction = (targetPositions[i]-unit.transform.position).normalized;
+                unit.rb.MovePosition(unit.transform.position + direction * unit.roamingSpeed);
+            }
         }
     }
 
--- Assets/Scripts/Units/AbaUnit.cs
+++ Assets/Scripts/Units/AbaUnit.cs
@@ -3,14 +3,26 @@ using PolyNav;
 
 namespace BioTower.Units
 {
+
+public enum AbaUnitState
+{
+    ROAMING,
+    CARRYING_ENEMY
+}
+
 [SelectionBase]
 public class AbaUnit : Unit
 {
+    public float roamingSpeed = 2.0f;
     public PolyNavAgent agent;
+    public AbaUnitState abaUnitState;
+    public Rigidbody rb;
+    [HideInInspector] public bool hasTargetRoamingPoint;
     
     private void Awake()
     {
         //agent.enabled = false;
+        abaUnitState = AbaUnitState.ROAMING;
     }
 
     public override void Start()

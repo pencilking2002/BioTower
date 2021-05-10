11d40c623def74b90c42999fb09dacde75ca5cda
DESKTOP-IH7CB2C\penci <pencilking2002@gmail.com>
2021-05-10 13:50:19 -0400

    Created a tool for branching road movement

---
+++ Assets/Scripts/Level/Waypoint.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+namespace BioTower.Level
+{
+public class Waypoint : MonoBehaviour
+{
+    public Waypoint nextWaypoint;
+    [ShowIf("isFork")]
+    public Waypoint nextWaypoint_02;
+    public Waypoint prevWaypoint;
+    public bool isFork;
+
+    private Vector3 GetTangent(Vector3 normal)
+    {
+        Vector3 tangent;
+        Vector3 t1 = Vector3.Cross(normal, Vector3.up);
+        Vector3 t2 = Vector3.Cross(normal, Vector3.forward);
+  
+        tangent = t1.sqrMagnitude > t1.sqrMagnitude ? t1 : t2;
+        return tangent;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = isFork ? Color.yellow : Color.red;
+
+        if (nextWaypoint != null)
+        {
+            Gizmos.DrawLine(transform.position, nextWaypoint.transform.position);
+
+            var arrowDir = (transform.position-nextWaypoint.transform.position).normalized * 0.5f;
+            var tangent = GetTangent(arrowDir) * 0.5f;
+            Gizmos.DrawLine(nextWaypoint.transform.position, nextWaypoint.transform.position + tangent + arrowDir);
+            Gizmos.DrawLine(nextWaypoint.transform.position, nextWaypoint.transform.position - tangent + arrowDir);
+
+        }
+
+        if (isFork && nextWaypoint_02 != null)
+        {
+            Gizmos.DrawLine(transform.position, nextWaypoint_02.transform.position);
+
+            var arrowDir = (transform.position-nextWaypoint_02.transform.position).normalized * 0.5f;
+            var tangent = GetTangent(arrowDir) * 0.5f;
+            Gizmos.DrawLine(nextWaypoint_02.transform.position, nextWaypoint_02.transform.position + tangent + arrowDir);
+            Gizmos.DrawLine(nextWaypoint_02.transform.position, nextWaypoint_02.transform.position - tangent + arrowDir);
+
+        }
+        
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, 0.2f);
+    }
+}
+}
---
+++ Assets/Scripts/Level/Waypoints.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace BioTower.Level
+{
+[ExecuteAlways]
+public class Waypoints : MonoBehaviour
+{
+
+    [MenuItem("Tools/Link Waypoints %l")]
+    private static void LinkWaypoints()
+    {
+        var selectedObjects = Selection.gameObjects;
+
+        if (selectedObjects.Length == 2)
+        {
+            if (selectedObjects[0].layer != 15 || selectedObjects[1].layer != 15)
+                return;
+            
+            var waypoint_01 = selectedObjects[1].GetComponent<Waypoint>();
+            var waypoint_02 = selectedObjects[0].GetComponent<Waypoint>();
+
+            waypoint_01.nextWaypoint = waypoint_02;
+            waypoint_02.prevWaypoint = waypoint_01;
+        }
+        else if (selectedObjects.Length == 3)
+        {
+            var waypoint_01 = selectedObjects[2].GetComponent<Waypoint>();
+            var waypoint_02 = selectedObjects[1].GetComponent<Waypoint>();
+            var waypoint_03 = selectedObjects[0].GetComponent<Waypoint>();
+
+            waypoint_03.isFork = true;
+            waypoint_03.nextWaypoint = waypoint_02;
+            waypoint_03.nextWaypoint_02 = waypoint_01;
+
+            waypoint_01.prevWaypoint = waypoint_03;
+            waypoint_02.prevWaypoint = waypoint_03;
+        }
+    }
+}
+}

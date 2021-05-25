b25fdf0bef9a6fe9c6802fbb7bd957a1f64a6fc0
DESKTOP-5EOJN0G\penci <pencilking2002@gmail.com>
2021-05-25 17:51:07 -0400

    Created X shape scene

--- Assets/ParadoxNotion/PolyNav2D/Scripts/Editor/PolyNav2DInspector.cs
+++ Assets/ParadoxNotion/PolyNav2D/Scripts/Editor/PolyNav2DInspector.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
@@ -23,3 +24,4 @@ namespace PolyNav
         }
     }
 }
+#endif
--- Assets/ParadoxNotion/PolyNav2D/Scripts/Editor/PolyNavObstacleInspector.cs
+++ Assets/ParadoxNotion/PolyNav2D/Scripts/Editor/PolyNavObstacleInspector.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEngine;
 using UnityEditor;
 using System.Collections;
@@ -50,3 +51,5 @@ namespace PolyNav
     }
 
 }
+
+#endif

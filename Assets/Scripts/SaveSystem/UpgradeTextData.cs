4d8191b310332d2db0907fc7ad115d7ebde05fd2
DESKTOP-IH7CB2C\penci <pencilking2002@gmail.com>
2021-09-10 17:31:21 -0400

    Created an SO for upgrade text lookup

---
+++ Assets/Scripts/SaveSystem/UpgradeTextData.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Sirenix.OdinInspector;
+
+namespace BioTower
+{
+
+[CreateAssetMenu(fileName="UpgradeTextData", menuName="UpgradeTextData")]
+public class UpgradeTextData : ScriptableObject
+{
+    [TabGroup("ABA")] public UpgradeData[] abaTowerDataArr;
+    [TabGroup("PP2C")] public UpgradeData[] ppc2TowerDataArr;
+    [TabGroup("Mito")] public UpgradeData[] mitoTowerDataArr;
+    [TabGroup("Chloroplast")] public UpgradeData[] chloroTowerDataArr;
+    [TabGroup("Player")] public UpgradeData[] playerTowerDataArr;
+
+
+    public UpgradeData GetUpgradeTextData(UpgradeType upgradeType)
+    {
+        foreach(UpgradeData data in abaTowerDataArr)
+        {
+            if (data.upgradeType == upgradeType) return data;
+        }
+        foreach(UpgradeData data in ppc2TowerDataArr)
+        {
+            if (data.upgradeType == upgradeType) return data;
+        }
+        foreach(UpgradeData data in mitoTowerDataArr)
+        {
+            if (data.upgradeType == upgradeType) return data;
+        }
+        foreach(UpgradeData data in chloroTowerDataArr)
+        {
+            if (data.upgradeType == upgradeType) return data;
+        }
+        foreach(UpgradeData data in playerTowerDataArr)
+        {
+            if (data.upgradeType == upgradeType) return data;
+        }
+        return null;
+    }
+
+}
+
+[Serializable]
+public class UpgradeData
+{
+    public UpgradeType upgradeType;
+    public string buttonText;
+    public string descrptionText;
+    public Sprite sprite;
+}
+}

b85cd7c3a915d9e27957f6d32b71f65a5a751042
DESKTOP-IH7CB2C\penci <pencilking2002@gmail.com>
2021-09-14 13:07:38 -0400

    Adds new sounds

--- Assets/Scripts/Util/AudioData.cs
+++ Assets/Scripts/Util/AudioData.cs
@@ -8,24 +8,25 @@ namespace BioTower
 [CreateAssetMenu(menuName ="AudioData", fileName="AudioData")]
 public class AudioData : ScriptableObject
 {
-    //[TabGroup("Music")] public AudioClip levelSelectTrack;
     //[TabGroup("Music")] public AudioClip newWave;
     [TabGroup("Music")] public AudioClip levelTrack;
     [TabGroup("Music")] public AudioClip gameOverLose;
     [TabGroup("Music")] public AudioClip gameOverWin;
+
     //[TabGroup("Music")] public AudioClip titleScreenTrack;
    
 
-    //[TabGroup("Units")] public AudioClip enemyCrystalPickedUp;
     //[TabGroup("Units")] public AudioClip unitDeath;
     //[TabGroup("Units")] public AudioClip unitDamage;
-    //[TabGroup("Units")] public AudioClip snrk2Spawned;
     //[TabGroup("Units")] public AudioClip snrk2LeavesTower;
-
+    [TabGroup("Units")] public AudioClip snrk2Spawned;
+    [TabGroup("Units")] public AudioClip enemyCrystalPickedUp;
+    [TabGroup("Units")] public AudioClip[] takeDamage;
     [TabGroup("Units")] public AudioClip enemyBaseAttacked;
     [TabGroup("Units")] public AudioClip abaSpawned;
     [TabGroup("Units")] public AudioClip crystalDeposited;
     [TabGroup("Units")] public AudioClip crystalPickedUp;
+    [TabGroup("Units")] public AudioClip explode;
 
 
     [TabGroup("Towers")] public AudioClip towerPlaced;
@@ -42,5 +43,11 @@ public class AudioData : ScriptableObject
     [TabGroup("UI")] public AudioClip btnSelected;
     [TabGroup("UI")] public AudioClip invalidBtnSelected;
     [TabGroup("UI")] public AudioClip letterRevealed;
+    [TabGroup("UI")] public AudioClip levelSelect;
+    [TabGroup("UI")] public AudioClip tutChat;
+    [TabGroup("UI")] public AudioClip titleDrop;
+    [TabGroup("UI")] public AudioClip tapAnywhere;
+    [TabGroup("UI")] public AudioClip energyGone;
+
 }
 }

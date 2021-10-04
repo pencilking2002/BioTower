89984e03ae45c1e893a2f8226d57b65fe5c693a6
Jenna Dancel <jdee0711@gmail.com>
2021-10-04 15:25:12 -0400

    Creates a pool manager and adds support for pooling light fragments

---
+++ Assets/Scripts/Managers/PoolManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace BioTower
+{
+public enum PoolObjectType
+{
+    NONE,
+    LIGHT_FRAGMENT
+}
+
+public class PoolManager : MonoBehaviour
+{
+    public List<PooledObject> pooledObjects;
+    [SerializeField] private GameObject lightFragPrefab;
+    [SerializeField] private Vector2 offscreenLocation;
+
+
+    public PooledObject GetPooledObject(PoolObjectType objectType)
+    {
+        PooledObject pooledObject = null;
+        int indexToRemove = -1;
+        for(int i=0; i<pooledObjects.Count; i++)
+        {
+            PooledObject obj = pooledObjects[i];
+            if (obj.objectType == objectType && !obj.isActive)
+            {
+                pooledObject = obj;
+                pooledObject.isActive = true;
+                indexToRemove = i;
+                break;
+            }
+        }
+
+        if (indexToRemove != -1)
+            pooledObjects.RemoveAt(indexToRemove);
+
+        else if (pooledObject == null)
+        {
+            pooledObject = CreatePrefab(objectType);
+            pooledObject.isActive = true;
+            pooledObjects.Add(pooledObject);
+        }
+        return pooledObject;
+    }
+
+    public void AddPooledObject(PooledObject pooledObj)
+    {
+        if (!pooledObjects.Contains(pooledObj))
+        {
+            pooledObjects.Add(pooledObj);
+            pooledObj.isActive = false;
+            pooledObj.transform.position = offscreenLocation;
+        }
+    }
+
+    private PooledObject CreatePrefab(PoolObjectType objType)
+    {
+        PooledObject obj = null;
+        switch(objType)
+        {
+            case PoolObjectType.LIGHT_FRAGMENT:
+                obj = Instantiate(lightFragPrefab).GetComponent<PooledObject>();
+                break;
+        }
+        return obj;
+    }
+}
+}
---
+++ Assets/Scripts/Util/PooledObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BioTower
+{
+public class PooledObject : MonoBehaviour
+{
+    public PoolObjectType objectType;
+    public bool isActive = true;
+
+    public bool IsLightFragment()
+    {
+        return objectType == PoolObjectType.LIGHT_FRAGMENT;
+    }
+
+    public LightFragment GetLightFragment()
+    {
+        return GetComponent<LightFragment>();
+    }
+}
+}

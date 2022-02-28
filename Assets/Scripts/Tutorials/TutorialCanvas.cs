f5335436e95907e8fb62f89139f7dd7583d1be63
MSI\penci <pencilking2002@gmail.com>
2022-02-28 18:52:48 -0500

    Starts implementing skip tutorial button

--- Assets/Scripts/Tutorials/TutorialCanvas.cs
+++ Assets/Scripts/Tutorials/TutorialCanvas.cs
@@ -1,88 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System;
-using UnityEngine.UI;
 using TMPro;
-using BioTower.Structures;
-using BioTower.Units;
 using Sirenix.OdinInspector;
+using UnityEngine.UI;
 
 namespace BioTower
 {
-public class TutorialCanvas : MonoBehaviour
-{
-    public static bool tutorialInProgress;
-    
-
-    [Header("Tut UI")]
-    public Transform tutPanel;
-    public Transform itemHighlightPanel;
-    public PortraitController portraitController;
-    public TextMeshProUGUI tutText;
-    public CanvasGroup ctaText;
-
-
-    [Header("Tut State")]
-    public bool hasTutorials;
-    [ShowIf("hasTutorials")] public TutState tutState;
-    [ShowIf("hasTutorials")]public int currTutorialIndex = -1;
-    [ShowIf("hasTutorials")]public TutorialData[] tutorials;
-    
-
-    [Header("Tut Animation")]
-    public int slideInOffset = 50;
-    public float revealDuration = 0.05f;
-
-    public TutorialData currTutorial => tutorials[currTutorialIndex];
-    [HideInInspector] public Canvas canvas;
-    [HideInInspector] public Vector3 initTutPanelLocalPos;
-    [HideInInspector] public WordAnimation tutTextWordAnim;
-    private Dictionary<TutState, TutStateBase> charStates = new Dictionary<TutState, TutStateBase>();
-
-    private void Awake()
+    public class TutorialCanvas : MonoBehaviour
     {
-        Util.tutCanvas = this;
-        canvas = GetComponent<Canvas>();
-        tutTextWordAnim = tutText.GetComponent<WordAnimation>();
-        CacheStates();
-    }
-
-    public bool IsLastTutorial(TutorialData data)
-    {
-        int index = Array.IndexOf(tutorials, data);
-        return index >= tutorials.Length-1;
-    }
+        public static bool tutorialInProgress;
 
 
-    private void CacheStates()
-    {
-        var states = GetComponentsInChildren<TutStateBase>();
-        foreach(TutStateBase state in states)
+        [Header("Tut UI")]
+        public Transform tutPanel;
+        public Transform itemHighlightPanel;
+        public PortraitController portraitController;
+        public TextMeshProUGUI tutText;
+        public CanvasGroup ctaText;
+        [SerializeField] private Button skipButton;
+
+
+        [Header("Tut State")]
+        public bool hasTutorials;
+        [ShowIf("hasTutorials")] public TutState tutState;
+        [ShowIf("hasTutorials")] public int currTutorialIndex = -1;
+        [ShowIf("hasTutorials")] public TutorialData[] tutorials;
+
+
+        [Header("Tut Animation")]
+        public int slideInOffset = 50;
+        public float revealDuration = 0.05f;
+
+        public TutorialData currTutorial => tutorials[currTutorialIndex];
+        [HideInInspector] public Canvas canvas;
+        [HideInInspector] public Vector3 initTutPanelLocalPos;
+        [HideInInspector] public WordAnimation tutTextWordAnim;
+        private Dictionary<TutState, TutStateBase> charStates = new Dictionary<TutState, TutStateBase>();
+
+        private void Awake()
         {
-            charStates.Add(state.tutState, state);
+            Util.tutCanvas = this;
+            canvas = GetComponent<Canvas>();
+            tutTextWordAnim = tutText.GetComponent<WordAnimation>();
+            CacheStates();
         }
-    } 
 
-    private void Update()
-    {
-        if (!GameManager.Instance.gameStates.IsGameState())
-            return;
+        public bool IsLastTutorial(TutorialData data)
+        {
+            int index = Array.IndexOf(tutorials, data);
+            return index >= tutorials.Length - 1;
+        }
 
-        tutState = charStates[tutState].OnUpdate(tutState);
+        private void CacheStates()
+        {
+            var states = GetComponentsInChildren<TutStateBase>();
+            foreach (TutStateBase state in states)
+            {
+                charStates.Add(state.tutState, state);
+            }
+        }
+
+        public void OnPressSkipTutButton()
+        {
+            if (!IsEndTutState())
+                SetEndTutState();
+        }
+
+        private void Update()
+        {
+            if (!GameManager.Instance.gameStates.IsGameState())
+                return;
+
+            tutState = charStates[tutState].OnUpdate(tutState);
+        }
+
+
+        public void SetNoneState() { tutState = TutState.NONE; }
+        public void SetLetterRevealState() { tutState = TutState.LETTER_REVEAL; }
+        public void SetWaitingTapState() { tutState = TutState.WAITING_TAP; }
+        public void SetWaitingButtonTapState() { tutState = TutState.WAITING_BUTTON_TAP; }
+        public void SetEndTutState() { tutState = TutState.END; }
+
+        public bool IsNoneState() { return tutState == TutState.NONE; }
+        public bool IsLetterRevealState() { return tutState == TutState.LETTER_REVEAL; }
+        public bool IsWaitingTapState() { return tutState == TutState.WAITING_TAP; }
+        public bool IsWaitingButtonTapState() { return tutState == TutState.WAITING_BUTTON_TAP; }
+        public bool IsEndTutState() { return tutState == TutState.END; }
     }
-
-    public void SetNoneState() { tutState = TutState.NONE; }
-    public void SetLetterRevealState() { tutState = TutState.LETTER_REVEAL; }
-    public void SetWaitingTapState() { tutState = TutState.WAITING_TAP; }
-    public void SetWaitingButtonTapState() { tutState = TutState.WAITING_BUTTON_TAP; }
-    public void SetEndTutState() { tutState = TutState.END; }
-
-    public bool IsNoneState() { return tutState == TutState.NONE; }
-    public bool IsLetterRevealState() { return tutState == TutState.LETTER_REVEAL; }
-    public bool IsWaitingTapState() { return tutState == TutState.WAITING_TAP; }
-    public bool IsWaitingButtonTapState() { return tutState == TutState.WAITING_BUTTON_TAP; }
-    public bool IsEndTutState() { return tutState == TutState.END; }
-
-}
 }

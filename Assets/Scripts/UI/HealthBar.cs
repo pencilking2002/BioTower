790732abcd05af752f311a295ac52430a78c4720
MSI\penci <pencilking2002@gmail.com>
2022-03-14 18:05:31 -0400

    Recoding the healthbar

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -11,14 +11,47 @@ namespace BioTower
         [SerializeField] private GameObject barPrefab;
         [SerializeField] private RectTransform healthPanel;
         private int health;
+        private RectTransform canvasRT;
+        private void Awake()
+        {
+            canvasRT = GetComponent<RectTransform>();
+            Init(10);
+            //Debug.Break();
+        }
 
         public void Init(int health)
         {
             this.health = health;
-            for (int i = 0; i < healthPanel.childCount; i++)
-            {
-                healthPanel.GetChild(i).gameObject.SetActive(i < health - 1);
+            // float sizeX = (canvasRT.sizeDelta.x * (health - 1)) / health;
+            // float offsetX = sizeX;
+            // for (int i = 0; i < health; i++)
+            // {
+            //     var bar = Instantiate(barPrefab);
+            //     bar.name = $"{bar.name}_{i}";
+            //     bar.transform.SetParent(healthPanel);
+            //     var rt = (RectTransform)bar.transform;
 
+            //     rt.sizeDelta = new Vector2(-sizeX, 0);
+            //     rt.anchoredPosition = new Vector2(-offsetX, 0);
+            //     offsetX -= sizeX;
+
+            //     // bar.transform.localPosition = Vector3.zero;
+            // }
+            Vector2 scale = new Vector2(1.0f / health, 1);
+            float halfCanvas = canvasRT.sizeDelta.x / 2;
+            float initialPosition = -halfCanvas + halfCanvas * scale.x;
+            float offsetX = initialPosition;
+            for (int i = 0; i < health; i++)
+            {
+                var go = Instantiate(barPrefab);
+                go.name = $"{go.name}_{i}";
+                go.transform.SetParent(healthPanel);
+                var rt = (RectTransform)go.transform;
+                go.transform.localPosition = new Vector2(offsetX, 0);
+                //go.TryGetComponent.localPosition += new Vector2(0,0);
+                rt.sizeDelta = Vector3.zero;
+                rt.localScale = scale;
+                //offsetX -= initialPosition * 2;
             }
         }
 

ffdf17d4066da008c1d6224e918fbb38dc5d5bdb
MSI\penci <pencilking2002@gmail.com>
2022-03-15 13:39:57 -0400

    Makes health bar work

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -10,37 +10,25 @@ namespace BioTower
     {
         [SerializeField] private GameObject barPrefab;
         [SerializeField] private RectTransform healthPanel;
+        [SerializeField] private Vector2 padding = new Vector2(0.5f, 0);
         private int health;
         private RectTransform canvasRT;
         private void Awake()
         {
             canvasRT = GetComponent<RectTransform>();
-            Init(10);
+            Init(5);
             //Debug.Break();
         }
 
         public void Init(int health)
         {
             this.health = health;
-            // float sizeX = (canvasRT.sizeDelta.x * (health - 1)) / health;
-            // float offsetX = sizeX;
-            // for (int i = 0; i < health; i++)
-            // {
-            //     var bar = Instantiate(barPrefab);
-            //     bar.name = $"{bar.name}_{i}";
-            //     bar.transform.SetParent(healthPanel);
-            //     var rt = (RectTransform)bar.transform;
-
-            //     rt.sizeDelta = new Vector2(-sizeX, 0);
-            //     rt.anchoredPosition = new Vector2(-offsetX, 0);
-            //     offsetX -= sizeX;
-
-            //     // bar.transform.localPosition = Vector3.zero;
-            // }
             Vector2 scale = new Vector2(1.0f / health, 1);
             float halfCanvas = canvasRT.sizeDelta.x / 2;
-            float initialPosition = -halfCanvas + halfCanvas * scale.x;
+
+            float initialPosition = (-halfCanvas + halfCanvas * scale.x);
             float offsetX = initialPosition;
+            float barSize = (halfCanvas * scale.x) * 2;
             for (int i = 0; i < health; i++)
             {
                 var go = Instantiate(barPrefab);
@@ -48,20 +36,11 @@ namespace BioTower
                 go.transform.SetParent(healthPanel);
                 var rt = (RectTransform)go.transform;
                 go.transform.localPosition = new Vector2(offsetX, 0);
-                //go.TryGetComponent.localPosition += new Vector2(0,0);
                 rt.sizeDelta = Vector3.zero;
                 rt.localScale = scale;
-                //offsetX -= initialPosition * 2;
+                offsetX += barSize;
             }
         }
-
-        // private GameObject CreateBar()
-        // {
-        //     var bar = Instantiate(barPrefab);
-        //     bar.transform.SetParent(healthPanel);
-        //     bar.transform.SetAsFirstSibling();
-        //     return bar;
-        // }
 
         public void SetHealth(int health)
         {
